75d22b6aa477315bdc4b61c79436995afa73d928
Jyoti <jyotiprashar1@gamil.com>
2022-02-28 20:03:26 +0530

    postgresql DB URI

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item,ItemList
 from resources.store import Store,StoreList
+from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,4 +24,11 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(Store,'/store/<string:name>')
 
 if __name__ == '__main__':
-    app.run(port=8000, debug = True)
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=8000)
